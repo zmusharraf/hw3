641704deb0cee1b53cd3211a3d4f0acde7a7a524
Zoya Musharraf <musharra@usc.edu>
2025-10-13 19:36:17 +0000

    Initial commit for HW3

---
+++ logicsim/logicsim.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+
+#include "circuit.h"
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+	stringstream ss;
+  Circuit C;
+  if(argc != 1)
+  {
+    if(C.parse(argv[1]))
+    {
+    	  C.startUml(ss);
+        C.run(ss);
+        C.endUml(ss);
+        cout << ss.str() << endl;
+    }
+  }
+  else
+  {
+      cout << "Please provide a circuit file to simulate." << endl;
+      return 1;
+  }
+}
